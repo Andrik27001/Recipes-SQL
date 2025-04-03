4b0f7bf2821936240dd40976cd0f914205307dd1
Andrik Solis <andrik27001@gmail.com>
2025-04-03 11:50:12 +0200

    Update 02_Crea_Dificultades.sql

--- ExamenBD/Recetas/02_Crea_Dificultades.sql
+++ ExamenBD/Recetas/02_Crea_Dificultades.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Dificultades (
 	Dificultad_id TINYINT IDENTITY(1,1) PRIMARY KEY,
 	Dificultad VARCHAR(10) UNIQUE NOT NULL,
-	CONSTRAINT Const_dificultad CHECK (Dificultad IN ('Muy f·cil', 'Elaborada', 'Complicada'))
+	CONSTRAINT Const_dificultad CHECK (Dificultad IN ('muy f√°cil', 'elaborada', 'complicada'))
 )
